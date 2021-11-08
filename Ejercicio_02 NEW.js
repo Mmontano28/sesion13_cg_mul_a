e3b61f81df8cf2f6da9b44388e1bcfb5a3a92003
mmontano28 <est.mateo.montano@unimilitar.edu.co>
2021-11-08 10:58:04 -0500

    MATEO MONTAÑO GAONA <est.mateo.montano@unimilitar.edu.co>

--- Ejercicio_02 NEW.js
+++ Ejercicio_02 NEW.js
@@ -42,17 +42,42 @@ function init()
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     //Mostrar ejes en la pantalla
-    var axes = new THREE.AxesHelper(40);
+    var axes = new THREE.AxesHelper(400);
     scene.add(axes);
 
     Cubo = [];   // Define un array unidimensional para contener los cubos
 
    
-    Dim=16; //Valor la inicial del cubo
+    //Dim=16; //Valor la inicial del cubo
+    Dim = prompt("Digite el tamaño del que cubo que desee", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
+
+    if(Angulo != null)//Este if se usa para generar un cuadro de texto alerta, para dar un mensaje dependiendo lo que el usuario interactue
+    {
+    alert("Su numero en radienes " + Radianes);
+    }   
+    else 
+    {
+    alert("No has escrito nada");
+    }
+
+    var Angulo = prompt("Digite el ángulo que desea en grados", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
+    Radianes=(Angulo)*((2*Math.PI)/(360));//Esta variable se usa para convertir de grados a radianes
+    if(Angulo != null)//Este if se usa para generar un cuadro de texto alerta, para dar un mensaje dependiendo lo que el usuario interactue
+    {
+    alert("Su numero en radienes " + Radianes);
+    }   
+    else 
+    {
+    alert("No has escrito nada");
+    }
+    
     Delta= Dim/2;//Valor el lado inicial del cubo dividido en 2
+    
+
     Diagonal=Math.sqrt(Math.pow(Delta,2)+Math.pow (Delta,2));//La diagonal es la variable que se usa para encontrar la longitud del centro del cubo, hacia el punto medio
-    Angulo=Math.PI/16;//Variable que proporciona el angulo de rotacion.
-    NewDelta=(Math.cos((Math.PI/4)-Angulo))*Diagonal;//Variable que determina el valor de traslacion que debe presentar el cubo
+
+    NewDelta=(Math.cos((Math.PI/4)-Radianes))*Diagonal;//Variable que determina el valor de traslacion que debe presentar el cubo
+
 
     Cubo.push(cubo(Dim, Dim, Dim, 0xFFDD00, 'Basic', false));//Se crea el primer cubo
     Cubo.push(cubo(Dim, Dim, Dim, 0x6FA8DC, 'Phong', false));//Se crea el segundo cubo
@@ -69,10 +94,11 @@ function init()
     
     for(i=1;i<3;i++) //Este for solo se toman los valores de 1 y 2 cubos, por lo tanto se trasladan hacia arriba mientras que tambirn su tamaño se escala a la mitad
     {
-        Tamano=((1)/(Dim/(Delta/i)));//Varible para escalar el tamaño respeto al cubo indicado, es decir el valor de la mitad del cubo anterior.
-        Altura=((Delta-2)*(1+i));//Altura Es una variable para cambiar la coordenada en y de los cubos. 
+        Tamano=1/(2*i);//Varible para escalar el tamaño respeto al cubo indicado, es decir el valor de la mitad del cubo anterior.
+        Altura=Dim/2+Dim/4+((((Dim/2)+(Dim/4))/2))*(i-1);//Altura Es una variable para cambiar la coordenada en y de los cubos. 
         Cubo[i].scale.set(Tamano,Tamano,Tamano); //Se escala las coordenas del cubo X,Y y Z que en este caso son la variable tamaño con un cambio pues se escala dos veces 
         Cubo[i].translateY(Altura);//El metodo que use fue el Metodo translate, con un parametro en este caso en la coordenada y 
+  
     }
     for(i=0;i<3;i++)//Este for se utiliza para mover unicamente los cubos 0 y 2, con respecto al eje y
     {
