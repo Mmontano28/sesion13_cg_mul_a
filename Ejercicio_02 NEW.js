b929e44e82f8302c6ca2d8dc297c5f226816a81d
mmontano28 <est.mateo.montano@unimilitar.edu.co>
2021-11-08 12:53:57 -0500

    MATEO MONTAÑO GAONA  <est.mateo.montano@unimilitar.edu.co>

--- Ejercicio_02 NEW.js
+++ Ejercicio_02 NEW.js
@@ -1,5 +1,6 @@
 // Se crea una escena, que contendrá todos nuestros elementos como objetos, cámaras y luces.
 var scene = new THREE.Scene();
+wireframe: false
 
 function cubo(x, y, z, color, material, alambrado)//Funcion por la cual contiene todos los metodos para crear un cubo, con las varibales de tamaño, color, material y alambrado
 {
@@ -49,39 +50,36 @@ function init()
 
    
     //Dim=16; //Valor la inicial del cubo
-    Dim = prompt("Digite el tamaño del que cubo que desee", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
 
-    if(Angulo != null)//Este if se usa para generar un cuadro de texto alerta, para dar un mensaje dependiendo lo que el usuario interactue
+    Dim = prompt("Digite el tamaño que desea para el cubo", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
+    if(Dim != null)//Este if se usa para generar un cuadro de texto alerta, para dar un mensaje dependiendo lo que el usuario interactue
     {
-    alert("Su numero en radienes " + Radianes);
+    alert("Su numero es "+ Dim);
     }   
     else 
     {
     alert("No has escrito nada");
     }
 
-    var Angulo = prompt("Digite el ángulo que desea en grados", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
-    Radianes=(Angulo)*((2*Math.PI)/(360));//Esta variable se usa para convertir de grados a radianes
+    Angulo = prompt("Digite el ángulo que desea en grados", " ejemplo. 25");//Se crea esta variable como cuadro de texto, donde al usuario le aparece un mensaje y a su vez la opcion de digitalizar un dato
     if(Angulo != null)//Este if se usa para generar un cuadro de texto alerta, para dar un mensaje dependiendo lo que el usuario interactue
     {
-    alert("Su numero en radienes " + Radianes);
+    alert("Su numero es "+ Angulo);
     }   
     else 
     {
     alert("No has escrito nada");
     }
-    
+
     Delta= Dim/2;//Valor el lado inicial del cubo dividido en 2
-    
-
     Diagonal=Math.sqrt(Math.pow(Delta,2)+Math.pow (Delta,2));//La diagonal es la variable que se usa para encontrar la longitud del centro del cubo, hacia el punto medio
-
+    //Angulo=Math.PI/16;//Variable que proporciona el angulo de rotacion.
+    Radianes=(Angulo)*((2*Math.PI)/(360));//Esta variable se usa para convertir de grados a radianes
     NewDelta=(Math.cos((Math.PI/4)-Radianes))*Diagonal;//Variable que determina el valor de traslacion que debe presentar el cubo
 
-
-    Cubo.push(cubo(Dim, Dim, Dim, 0xFFDD00, 'Basic', false));//Se crea el primer cubo
+    Cubo.push(cubo(Dim, Dim, Dim, 0xFFDD00, 'Phong', false));//Se crea el primer cubo
     Cubo.push(cubo(Dim, Dim, Dim, 0x6FA8DC, 'Phong', false));//Se crea el segundo cubo
-    Cubo.push(cubo(Dim, Dim, Dim, 0x0000FF, 'Basic', false));//Se crea el tercer cubo
+    Cubo.push(cubo(Dim, Dim, Dim, 0x0000FF, 'Lambert', false));//Se crea el tercer cubo
     
   
     for(i=0;i<3;i++)//Este for sirve para transaladar los cubos al origen de las coordenadas, es decir, al lugar indicado, el valor de delta
@@ -94,24 +92,24 @@ function init()
     
     for(i=1;i<3;i++) //Este for solo se toman los valores de 1 y 2 cubos, por lo tanto se trasladan hacia arriba mientras que tambirn su tamaño se escala a la mitad
     {
-        Tamano=1/(2*i);//Varible para escalar el tamaño respeto al cubo indicado, es decir el valor de la mitad del cubo anterior.
+        Tamano=((1)/(Dim/(Delta/i)));//Varible para escalar el tamaño respeto al cubo indicado, es decir el valor de la mitad del cubo anterior.
         Altura=Dim/2+Dim/4+((((Dim/2)+(Dim/4))/2))*(i-1);//Altura Es una variable para cambiar la coordenada en y de los cubos. 
         Cubo[i].scale.set(Tamano,Tamano,Tamano); //Se escala las coordenas del cubo X,Y y Z que en este caso son la variable tamaño con un cambio pues se escala dos veces 
         Cubo[i].translateY(Altura);//El metodo que use fue el Metodo translate, con un parametro en este caso en la coordenada y 
-  
     }
     for(i=0;i<3;i++)//Este for se utiliza para mover unicamente los cubos 0 y 2, con respecto al eje y
     {
         if(i==0 || i==2)
         {
-            Cubo[i].rotateY(Angulo);
+            Cubo[i].rotateY(Radianes);
         }
     }
+   
     
     //Luz (requerida para el material MeshLambertMaterial)
     light = new THREE.PointLight(0xFFFFFF); //  Luz proveniente de un punto en el espacio, 
                                             //  semejante al sol.
-    light.position.set(30, 5, 30);          //  Localización de la luz. (x, y, z).
+    light.position.set(-10, 50, 30);          //  Localización de la luz. (x, y, z).
     scene.add( light ); 
 
     //Posicione y apunte la camara al centro de la escena 
